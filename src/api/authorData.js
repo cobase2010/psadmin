4eff0a923ec66a25bc70549219461c8b9806ef15
cobase2010 <cobase2010@live.com>
2016-11-11 16:09:41 -0800

    Changed to use react router.

---
+++ src/api/authorData.js
@@ -0,0 +1,25 @@
+module.exports = {
+  authors:
+  [
+    {
+      id: 'cory-house',
+      firstName: 'Cory',
+      lastName: 'House'
+    },
+    {
+      id: 'scott-allen',
+      firstName: 'Scott',
+      lastName: 'Allen'
+    },
+    {
+      id: 'dan-wahlin',
+      firstName: 'Dan',
+      lastName: 'Wahlin'
+    },
+    {
+      id: 'mark-young',
+      firstName: 'Mark',
+      lastName: 'Young'
+    }
+  ]
+}
